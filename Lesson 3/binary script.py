275f2b92b6f9017382250d4a4ca5fb97912ddd32
Rob-Conan <Rob-Conan@users.noreply.github.com>
2014-05-23 01:08:14 +0100

    Update binary script.py

--- Lesson 3/binary script.py
+++ Lesson 3/binary script.py
@@ -14,18 +14,18 @@ if len(sys.argv) == 2:
 	ip_addr = sys.argv.pop()
 	octets = ip_addr.split('.')
 	
-	if len(octets) != 4:					# Some crude error checking
+	if len(octets) != 4:						# Some crude error checking
 		sys.exit("Please enter a valid IP address")
 
 	a = range(4)							# Create a list of [0,1,2,3]
-	b = a[0:]								# Create a copy of the original list
+	b = a[0:]							# Create a copy of the original list
 	for i,enum in enumerate(octets):
-		a[i] = bin(int(enum))				# Change list to be the binary of our input
-#		print a[i]							# Print for debugging purposes
+		a[i] = bin(int(enum))					# Change list to be the binary of our input
+#		print a[i]						# Print for debugging purposes
 		b[i] = a[i][2:]						# Remove the first two "bytes" (0b)
 		while len(b[i]) <= 8:
 			b[i] = '0' + b[i]
-#		print b[i];							# Print for debugging purposes
+#		print b[i];						# Print for debugging purposes
 
 	bin_addr = ".".join(b)
 	print "%-20s %-20s" % ("IP Address", "Binary Address")
